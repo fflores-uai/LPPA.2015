688b5402fe4051c2cfc941060de75ab77ba6747d
floresfanelli <francisco.floresfanelli@gmail.com>
2015-07-14 23:21:10 -0300

    Update ResumeXML

--- LPPAWeb/Entitys/Entitys/ResumeXML.vb
+++ LPPAWeb/Entitys/Entitys/ResumeXML.vb
@@ -1,13 +1,53 @@
-﻿Public Class ResumeXML
+﻿Namespace Entitys
+
+    Public Class ResumeXML
 
 
 
-End Class
+    End Class
 
-Public Class Telephone
-    Property Type As String
-    Property IntCode As Integer
-    Property AreaCode As Integer
-    Property Number As String
+    Public Class Employement
 
-End Class
+        Property StartDate As Date
+        Property EndDate As Date
+        Property OrgName As String
+        Property JobTitle As String
+        Property Responsibility As String
+        Property FunctionCategory As String
+        Property IndustryCategory As String
+        Property Location As Location
+    End Class
+
+    Public Class Education
+
+        Property Level As String
+        Property StartDate As Date
+        Property EndDate As Date
+        Property Degree As String
+        Property Major As String
+        Property Minor As String
+        Property GPA As Double
+        Property GPAScale As Double
+        Property School As String
+
+    End Class
+
+
+    Public Class Location
+
+        Property CountryRegion As String
+        Property State As String
+        Property City As String
+
+    End Class
+
+    Public Class Telephone
+
+        Property Type As String
+        Property IntCode As Integer
+        Property AreaCode As Integer
+        Property Number As String
+
+    End Class
+
+End Namespace
